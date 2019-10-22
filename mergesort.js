958db581cc107fe8d80c610ae68e675b2da904a8
Chris Chin <chin.christopher@gmail.com>
2019-10-22 16:25:40 -0400

    added comparator functionality

--- mergesort.js
+++ mergesort.js
@@ -1,48 +1,48 @@
 function split(wholeArray) {
-  const midpoint = Math.floor(wholeArray.length / 2)
+  const midpoint = Math.floor(wholeArray.length / 2);
 
-  const firstHalf = wholeArray.slice(0, midpoint)
-  const secondHalf = wholeArray.slice(midpoint)
+  const firstHalf = wholeArray.slice(0, midpoint);
+  const secondHalf = wholeArray.slice(midpoint);
 
-  return [firstHalf, secondHalf]
+  return [firstHalf, secondHalf];
 }
 
-function merge(arr1, arr2) {
-  let mergedArr = []
-  let index1 = 0
-  let index2 = 0
+function merge(arr1, arr2, comparator = (a, b) => a - b < 0) {
+  let mergedArr = [];
+  let index1 = 0;
+  let index2 = 0;
 
   if (arr1.length === 1 && arr2.length === 1) {
-    if (arr1[0] < arr2[0]) {
-      mergedArr.push(arr1[0])
-      mergedArr.push(arr2[0])
+    if (comparator(arr1[0], arr2[0])) {
+      mergedArr.push(arr1[0]);
+      mergedArr.push(arr2[0]);
     } else {
-      mergedArr.push(arr2[0])
-      mergedArr.push(arr1[0])
+      mergedArr.push(arr2[0]);
+      mergedArr.push(arr1[0]);
     }
   } else {
     while (index1 + index2 < arr1.length + arr2.length) {
-      if (arr1[index1] < arr2[index2]) {
-        mergedArr.push(arr1[index1])
+      if (comparator(arr1[index1], arr2[index2])) {
+        mergedArr.push(arr1[index1]);
         // if (index1 !== arr1.length - 1)
         // if (index1 <= arr1.length - 2)
-        index1++
+        index1++;
       } else {
-        mergedArr.push(arr2[index2])
-        index2++
+        mergedArr.push(arr2[index2]);
+        index2++;
       }
     }
   }
-  return mergedArr
+  return mergedArr;
 }
 
-function mergeSort(array) {
+function mergeSort(array, comparator = (a, b) => a - b < 0) {
   //base case 1: 0 elements:
-  if (array.length === 0) return []
+  if (array.length === 0) return [];
   //base case 2: 1 element:
-  else if (array.length === 1) return array
+  else if (array.length === 1) return array;
   else {
-    const [firstHalf, secondHalf] = split(array)
-    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    const [firstHalf, secondHalf] = split(array);
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf), comparator);
   }
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,26 +1,27 @@
-describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
-    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]])
-  })
-  it('is able to split an uneven array into two halves', function() {
-    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]])
-  })
-})
+describe("Split Array function", function() {
+  it("is able to split an array into two halves", function() {
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+  });
+  it("is able to split an uneven array into two halves", function() {
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2], [3, 4, 5]]);
+  });
+});
 
-describe('Merge function', function() {
-  it('is able to merge two sorted arrays into one sorted array', function() {
-    expect(merge([1, 3, 5, 7], [2, 4, 6, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8])
-  })
-  it('2 singletons', function() {
-    expect(merge([3], [1])).toEqual([1, 3])
-  })
-})
+describe("Merge function", function() {
+  it("is able to merge two sorted arrays into one sorted array", function() {
+    expect(merge([1, 3, 5, 7], [2, 4, 6, 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    // expect(merge([8, 6, 4, 2], [7, 5, 3, 1])).toEqual([8, 7, 6, 5, 4, 3, 2, 1]);
+  });
+  it("2 singletons", function() {
+    expect(merge([3], [1])).toEqual([1, 3]);
+  });
+});
 
-describe('Mergesort', function() {
-  it('handles empty arrays', function() {
-    expect(mergeSort([])).toEqual([])
-  })
-  it('sorts the array', function() {
+describe("Mergesort", function() {
+  it("handles empty arrays", function() {
+    expect(mergeSort([])).toEqual([]);
+  });
+  it("sorts the array", function() {
     expect(mergeSort([3, 1, 5, 7, 2, 4, 6, 8])).toEqual([
       1,
       2,
@@ -30,9 +31,9 @@ describe('Mergesort', function() {
       6,
       7,
       8
-    ])
-  })
-  it('sorts a harder array with duplicates', function() {
-    expect(mergeSort([3, 2, 8, 14, 14])).toEqual([2, 3, 8, 14, 14])
-  })
-})
+    ]);
+  });
+  it("sorts a harder array with duplicates", function() {
+    expect(mergeSort([3, 2, 8, 14, 14])).toEqual([2, 3, 8, 14, 14]);
+  });
+});
